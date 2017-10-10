029e3c88767f90ee5ec3e7f9d2a4013b9c9c91c5
deviantfero <fmorataya.04@gmail.com>
2017-10-09 18:52:24 -0600

    read user input and send messages to sfs

--- src/sfclient.c
+++ src/sfclient.c
@@ -1,6 +1,53 @@
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "comms.h"
+#include "menu.h"
 
-int main(int argc, char *argv[]) {
-	printf("hello client\n");
+
+int main(void) {
+	printf(":: Simple File Client\n");
+
+	const char* fifo_path = "/tmp/fifo";
+
+	struct options *active_opts = get_default_opts();
+	enum menu_opt opt;
+
+	while(opt != EXIT) {
+		opt = run_menu(active_opts);
+		system("clear");
+		switch(opt) {
+			case SERVER_LS:
+				send_message(fifo_path, MSG_LS, true);
+				// wait message in process fifo file
+				/* wait_message("/tmp/fifo2"); */
+				break;
+			case SERVER_STATE:
+				//call server state here
+				break;
+			case UPLD_FILE:
+				//upload file
+				break;
+			case DWNLD_FILE:
+				//download file
+				break;
+			case TOGGLE_ENCRYPTION:
+				active_opts->encrypt = !active_opts->encrypt;
+				break;
+			case TOGGLE_COMPRESSION:
+				active_opts->compress = !active_opts->compress;
+				break;
+			case NOT_VALID: break;
+			case EXIT: 
+				send_message(fifo_path, MSG_EXIT, true);
+				exit(0);
+		}
+
+	}
 	return 0;
 }
