c3fe01c395f7ffeb136cd4fcf5e377eda91a87e0
deviantfero <fmorataya.04@gmail.com>
2017-11-04 02:17:38 -0600

    reduce uses of MAX_BUFFER by getting messages by byte and getting buffer size before snprintf

--- src/comms.c
+++ src/comms.c
@@ -7,22 +7,40 @@ void send_message(const char *pipe_name, char *msg, bool do_unlink) {
 	mkfifo(pipe_name, 0666);
 
 	int fifod = open(pipe_name, O_WRONLY);
-	char *processed_message = malloc(MAX_BUFFER);
+	int pm_size = buffer_size("%s;%d", msg, getpid());
+	char *processed_message = malloc(pm_size);
 
-	snprintf(processed_message, MAX_BUFFER, "%s;%d", msg, getpid());
-	write(fifod, processed_message, MAX_BUFFER);
+	snprintf(processed_message, pm_size, "%s;%d", msg, getpid());
+	write(fifod, processed_message, pm_size);
 	close(fifod);
 
 	if(do_unlink) unlink(pipe_name);
 }
 
-char *wait_message(const char *pipe_name) {
-	char* msg_buffer = malloc(MAX_BUFFER);
+char **wait_message(const char *pipe_name) {
+	char *msg_buffer = malloc(1);
+	char byte = 0;
+	char **msg = malloc(sizeof(char*) * MAX_TOKENS);
+	char *token;
+	int count = 0;
+
+	if(msg_buffer == NULL || msg == NULL) {
+		return NULL;
+	}
 
 	int fifod = open(pipe_name, O_RDONLY);
-	read(fifod, msg_buffer, MAX_BUFFER);
-	close(fifod);
 
-	return msg_buffer;
+	while(read(fifod, &byte, 1) == 1) {
+		msg_buffer = realloc(msg_buffer, count  + 1);
+		msg_buffer[count] = byte;
+		count++;
+	}
+
+	for(int i = 0; (token = strsep(&msg_buffer, ";")) != NULL && i < MAX_TOKENS; i++)
+		msg[i] = token;
+
+	close(fifod);
+	free(msg_buffer);
+	return msg;
 }
 
--- src/comms.h
+++ src/comms.h
@@ -1,26 +1,33 @@
 #ifndef COMMS_H
 
 #include <string.h>
+#include <stropts.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include "utils.h"
 
 #define COMMS_H
-#define MAX_BUFFER 1024
+#define MAX_TOKENS 10
+#define MAX_BUFFER 9000
 #define MSG_LS     "ls"
 #define MSG_STATUS "status"
 #define MSG_UPLD   "upload"
+#define MSG_UPLD_E "eupload"
+#define MSG_UPLD_C "cupload"
 #define MSG_DOWNLD "download"
 #define MSG_EXIT   "bye"
 #define MSG_ARRIVE "hello"
 
 /* messages should follow format "what;pid" */
 void send_message(const char *pipe_name, char *msg, bool do_unlink);
-char *wait_message(const char *pipe_name);
+char **wait_message(const char *pipe_name);
+char **parse_message(const char *msg);
 
 #endif /* ifndef COMMS_H */
