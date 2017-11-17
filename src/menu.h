9b5082a9dfb966c4db7aca6f0e82df3b57a7a789
deviantfero <fmorataya.04@gmail.com>
2017-11-16 23:40:02 -0600

    add methods to enum, add SET_MODE menu opt

--- src/menu.h
+++ src/menu.h
@@ -19,20 +19,30 @@ enum menu_opt {
 	TOGGLE_ENCRYPTION,
 	TOGGLE_COMPRESSION,
 	CLIENT_LS,
+	SET_MODE,
 	EXIT
 };
 
 struct options {
 	bool encrypt;
 	bool compress;
+	int chunk_size;
 	int method;
 };
 
-void print_menu(struct options *copt);
-void info_screen(char *info);
-int run_menu(struct options *copt);
+enum method {
+	PIPES,
+	QUEUE,
+	SOCKETS
+};
+
+char* get_method_name(enum method m );
 int choose_file(const char *dir_status, int file_count);
+int method_menu();
+int run_menu(struct options *copt);
 struct options *get_default_opts();
+void info_screen(char *info);
+void print_menu(struct options *copt);
 
 #define MENU_H
 #endif /* ifndef MENU_H */
