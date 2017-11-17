fd000bfe627e12e0f6821268f23d65eaa04aaa37
deviantfero <fmorataya.04@gmail.com>
2017-11-16 23:39:16 -0600

    add method menu, add chunk-size to options, later to be set

--- src/menu.c
+++ src/menu.c
@@ -11,8 +11,33 @@ void print_menu(struct options *copt) {
 	printf("\t5. Encrypt transfer %s\n", encryption_entry);
 	printf("\t6. Compress transfer %s\n", compression_entry);
 	printf("\t7. See client content\n");
-	puts("\t8. Exit");
+	printf("\t8. Set transfer method [%s]\n", get_method_name(copt->method));
+	puts("\t9. Exit");
 	printf(PROMPT);
+}
+
+char* get_method_name(enum method m) {
+	switch(m) {
+		case PIPES:   return "pipes";
+		case QUEUE:   return "queue";
+		case SOCKETS: return "sockets";
+		default:      return "unknown";
+	}
+}
+
+int method_menu() {
+	enum method m;
+	char* tmp = malloc(BUFFER_MAX);
+	if(tmp == NULL) return PIPES;
+
+	do {
+		for(int i = 0; i <= SOCKETS; i++)
+			fprintf(stdout, "[%d] %s\n", i, get_method_name(i));
+		fprintf(stdout, "\nEnter a method number: ");
+		m = atoi(fgets(tmp, BUFFER_MAX, stdin));
+	} while(m < PIPES || m > SOCKETS);
+
+	return m;
 }
 
 int run_menu(struct options *copt) {
@@ -55,6 +80,7 @@ struct options *get_default_opts() {
 	default_opts->encrypt = false;
 	default_opts->compress = false;
 	default_opts->method = 0;
+	default_opts->chunk_size = 0;
 
 	return default_opts;
 }
